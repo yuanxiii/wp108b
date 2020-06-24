1588c230d46ff21e4ac53a2c8bc4541cc805cc60
yuanxiii <65764476+yuanxiii@users.noreply.github.com>
2020-06-25 01:36:35 +0800

    Create vectortest.js

---
+++ hw6/vectortest.js
@@ -0,0 +1,6 @@
+const Vector = require('./vector')
+var v1 = new Vector([1,2,3])
+var v2 = new Vector([1,1,1])
+console.log('v1.add(v2)=', v1.add(v2))
+console.log('v1.dot(v2)=', v1.dot(v2))
+console.log('v1.neg()=', v1.neg())
